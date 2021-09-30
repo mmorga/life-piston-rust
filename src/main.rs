80e954b71a171ddfeac0a4029049ae7437652bf5
Mark Morga <markmorga@gmail.com>
2021-09-30 18:15:51 -0500

    window name change

--- src/main.rs
+++ src/main.rs
@@ -112,7 +112,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", WINDOW_SIZE)
+    let mut window: Window = WindowSettings::new("game-of-life", WINDOW_SIZE)
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
